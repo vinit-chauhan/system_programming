721480f02872a52308080c2e123d66dc88ed6e36
vinit-chauhan <chauhanvinit23@gmail.com>
2023-09-22 18:56:02 -0400

    Added Runner utility

---
+++ run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+arg=$1
+
+input_file=${arg/.c/}
+output_file=outputs/$input_file.out
+path=''
+
+# Split the string by /
+readarray -d "/" -t dirs <<< "$output_file"
+
+for ((n=0; n < ${#dirs[*]} - 1; n++)) do
+    path=${path}/${dirs[n]}
+done
+
+mkdir -p ./$path
+
+gcc $input_file.c -o ./${output_file}
+
+./${output_file}
