2a9e22dc02527669cea554fe7b45d3165b8372f0
vinit-chauhan <chauhanvinit23@gmail.com>
2023-09-24 18:03:07 -0400

    Added code for malloc()

---
+++ extras/dynamic_memory_allocation.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n;
+
+    scanf("%d", &n);
+    int *ptr;
+
+    ptr = malloc(n * sizeof(int)); // n is the number of elements in the array
+
+    if (ptr == NULL)
+    {
+        printf("Memory not allocated.\n");
+        exit(0);
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("Enter element %d: ", i);
+        scanf("%d", ptr + i);
+    }
+
+    printf("Elements of array are: [");
+    for (int i = 0; i < n; i++)
+    {
+        if (i == n - 1)
+            printf("%d]\n", *(ptr + i));
+        else
+            printf("%d, ", *(ptr + i));
+    }
+
+    return 0;
+}
