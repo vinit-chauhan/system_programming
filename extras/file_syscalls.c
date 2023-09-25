7c5eda6a839d13a83c445de7b3d3c056ba758020
vinit-chauhan <chauhanvinit23@gmail.com>
2023-09-25 09:31:41 -0400

    dynamic allocation of buffer

--- extras/file_syscalls.c
+++ extras/file_syscalls.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
 
@@ -14,10 +15,19 @@ int main()
     if ((fd = open("data/lorem_ipsum.txt", O_RDONLY)) == -1)
     {
         write(STDERR_FILENO, "Error opening file.\n", 20);
+        exit(1);
     }
     else
     {
-        char buffer[100];
+        // default size is 8 bytes
+        char *buffer;
+
+        if ((buffer = malloc(100 * sizeof(char))) == NULL)
+        {
+            write(STDERR_FILENO, "Error allocating memory.\n", 25);
+            exit(1);
+        }
+
         int bytes_read;
         do
         {
@@ -25,16 +35,19 @@ int main()
             if (bytes_read == -1)
             {
                 write(STDERR_FILENO, "Error reading file.\n", 20);
+                exit(1);
             }
             else if (bytes_read > 0)
             {
                 write(STDOUT_FILENO, buffer, bytes_read);
             }
         } while (bytes_read != 0 || bytes_read == -1);
+        write(STDOUT_FILENO, "\n", 1);
 
         if (close(fd) == -1)
         {
             write(STDERR_FILENO, "Error closing file.\n", 20);
+            exit(1);
         }
     }
 
