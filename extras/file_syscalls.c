cf146bcf385f0a4ddb3e7f2ba6449f5a66abd5d8
vinit-chauhan <chauhanvinit23@gmail.com>
2023-09-25 08:50:37 -0400

    Inline open

--- extras/file_syscalls.c
+++ extras/file_syscalls.c
@@ -9,8 +9,8 @@ int main()
     printf("%d\n", STDOUT_FILENO);
     printf("%d\n", STDERR_FILENO);
 
-    int fd = open("data/lorem_ipsum.txt", O_RDONLY);
-    if (fd == -1)
+    int fd;
+    if ((fd = open("data/lorem_ipsum.txt", O_RDONLY)) == -1)
     {
         write(STDERR_FILENO, "Error opening file.\n", 20);
     }
