0ed6553a0230c963f3f2ae5b194820397166640d
vinit-chauhan <chauhanvinit23@gmail.com>
2023-09-28 00:00:14 -0400

    Added logic to store args and added Manual page

--- assignments/assignment-1/dircpmvlist.c
+++ assignments/assignment-1/dircpmvlist.c
@@ -3,11 +3,58 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+void
+print_manual(int output_fd) {
+    char page[] = "Usage: dircpmvlist SOURCE DEST OPTIONS [EXTENSIONS]   \n\
+    \n\
+Options:    \n\
+    -cp: Copy files from SOURCE to DEST  \n\
+    -mv: Move files from SOURCE to DEST  \n\
+    \n\
+Extensions: (Upto 6 extensions)     \n\
+    List of extensions to copy/move \n\
+    ex. -cp .txt .pdf .docx \n\
+    \n\
+Example:   \n\
+    $ dircpmvlist /home/user/Downloads /home/user/Documents -cp .txt .pdf .docx \n\
+    $ dircpmvlist /home/user/Downloads /home/user/Documents -mv .txt .pdf .docx \n";
+
+    write(output_fd, page, sizeof(page));
+}
+
 int
 main(int argc, char* argv[]) {
-    for (int i = 1; i < argc; i++) {
-        printf("argv[%d] = %s\n", i, argv[i]);
+    char *source_dir, *destination_dir, *options;
+    char** extensions;
+    int OUTPUT = STDOUT_FILENO;
+
+    // Check for number of arguments and throw error if not in range
+    if (argc < 4) {
+        write(STDERR_FILENO, "[Error]: Too few arguments.\n", 28);
+        print_manual(OUTPUT);
+        exit(001);
+    } else if (argc > 10) {
+        write(STDERR_FILENO, "[Error]: Too many arguments.\n", 29);
+        print_manual(OUTPUT);
+        exit(002);
     }
 
-    nftw(argv[1], display_info, 20, 0);
+    // Parse command line arguments
+    source_dir = argv[1];
+    destination_dir = argv[2];
+    options = argv[3];
+
+    // iterate through all extensions and store them in an array
+    extensions = malloc((argc - 4) * sizeof(char*));
+    for (int i = 4; i < argc; i++) {
+        extensions[i - 4] = argv[i];
+    }
+
+    printf("Source: %s\n", source_dir);
+    printf("Destination: %s\n", destination_dir);
+    printf("Options: %s\n", options);
+    printf("Extensions: ");
+    for (int i = 0; i < argc - 4; i++) {
+        printf("%s ", extensions[i]);
+    }
 }
