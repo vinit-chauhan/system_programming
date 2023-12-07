af58e5628d26a9b93e7ad0254573fe6fee613f2e
vinit-chauhan <chauhanvinit23@gmail.com>
2023-12-07 00:24:39 -0500

    Working for 1st iteration.

--- assignments/assignment-4/A4F23.sh
+++ assignments/assignment-4/A4F23.sh
@@ -2,8 +2,8 @@
 
 echo "Backup script started"
 
-cb_counter=0
-ib_counter=0
+cb_counter=1
+ib_counter=1
 
 home_dir=$HOME
 
@@ -18,7 +18,7 @@ backup_tmp_dir=$backup_dir/_tmp
 
 cd $home_dir
 
-iterate_directory() {
+cb_iterate_directory() {
     # iterate over all files in home directory
     for file in $(ls $1); do
 
@@ -39,7 +39,7 @@ iterate_directory() {
             cd $file
 
             # iterate over all files in directory
-            iterate_directory $temp
+            cb_iterate_directory $temp
 
         elif [ -f $file ]; then
             # if file is .c or .txt then backup
@@ -48,6 +48,30 @@ iterate_directory() {
             fi
         fi
         cd $1
+    done
+}
+
+ib_iterate_directory() {
+    cru_dir=$1
+    # iterate over all files in home directory
+    for file in $(find $1 -type f -newer "$backup_dir/$name"); do
+
+        temp=${file##"$home_dir/"}
+
+        if [[ $temp == backup/* ]]; then
+            continue
+        fi
+
+        temp=$backup_tmp_dir${file#$home_dir}
+
+        mkdir -p ${temp%/*}
+
+        if [[ $file == *.c || $file == *.txt ]]; then
+            cp $file ${temp%/*}
+        fi
+
+        # echo $backup_tmp_dir${file#$home_dir}
+
     done
 }
 
@@ -60,14 +84,15 @@ generate_file_name() {
 }
 
 complete_backup() {
-
-    iterate_directory $home_dir
+    # iterate over all files in home directory
+    cb_iterate_directory $home_dir
 
     # generate backup file name
     name=$(generate_file_name cb)
 
     # create tar file and then remove backup tmp directory
-    tar -czf $backup_dir/$name -C $backup_tmp_dir * && rm -rf $backup_tmp_dir
+    cd $backup_tmp_dir &&
+        tar -cz -f $backup_dir/$name * && cd $home_dir && rm -rf $backup_tmp_dir
 
     # create backup log entry
     echo $(date) $name was created >>$backup_dir/backup.log
@@ -78,45 +103,32 @@ complete_backup() {
 
 incremental_backup() {
 
-    # find files that changed after the last backup
-    files=$(find $home_dir -type f -newer $backup_dir/backup.log)
+    # TODO: get newest file among all files in backup directory
+    ib_name=$(generate_file_name ib)
+    if [ ! -f $backup_dir/$ib_name ]; then
+        name=$(generate_file_name cb)
+    else
+        name=$ib_name
+    fi
 
-    # iterate over all files that changed
-    for file in $files; do
+    if [ $(find $home_dir -type f -newer "$backup_dir/$name" | wc -l) -gt 0 ]; then
 
-        temp=$(realpath $file)
+        # iterate over all files in home directory
+        ib_iterate_directory $home_dir
 
-        # check if file is a directory
-        if [ -d $file ]; then
+        # create tar file and then remove backup tmp directory
+        cd $backup_tmp_dir &&
+            tar -cz -f $backup_dir/$ib_name * && cd $home_dir && rm -rf $backup_tmp_dir
 
-            # check if directory is backup directory
-            # if yes then skip
-            if [ $temp == $exclude_dir ]; then
-                continue
-            fi
+        # add entry to backup log
+        echo $(date) $ib_name 'was created' >>$backup_dir/backup.log
 
-            # create directory in backup tmp directory
-            mkdir -p $backup_tmp_dir${temp#"$home_dir"}
+        # increment incremental backup counter
+        ib_counter=$((ib_counter + 1))
+    else
+        echo $(date) No changes-Incremental backup was not created >>$backup_dir/backup.log
+    fi
 
-            cd $file
-
-            # iterate over all files in directory
-            iterate_directory $temp
-
-        elif [ -f $file ]; then
-            # if file is .c or .txt then backup
-            if [[ $file == *.c || $file == *.txt ]]; then
-                cp $file $backup_tmp_dir${temp#"$home_dir"}
-            fi
-        fi
-        cd $1
-    done
-
-    # create tar file
-    tar -czf $backup_dir/incremental_backup.tar.gz -C $backup_tmp_dir *
-
-    # remove backup tmp directory
-    rm -rf $backup_tmp_dir
 }
 
 # check if backup directory exists
@@ -136,26 +148,11 @@ fi
 
 # check if backup is required
 if [ ! -f $backup_dir/$(generate_file_name cb) ]; then
-
+    echo "Creating Complete Backup"
     complete_backup
-
 else
-
-    name=$(generate_file_name ib)
-
-    if [ $(find $home_dir -type f -newer "$backup_dir/$name" | wc -l) -gt 0 ]; then
-        incremental_backup
-        echo $(date) $name 'was created' >>$backup_dir/backup.log
-    else
-        echo $(date) No changes-Incremental backup was not created >>$backup_dir/backup.log
-    fi
+    echo "Creating Incremental Backup"
+    incremental_backup
 fi
 
 echo done
-
-# # filees that changed after backup
-# find $backup_tmp_dir -type f -newer $backup_dir/backup.log -exec echo {} \; > $backup_dir/backup.log
-
-# touch -t 201112220000 start
-# touch -t 201112240000 stop
-# find . -newer start \! -newer stop
